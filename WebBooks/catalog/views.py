9fe902ae740d50be4b4dbd5e7fe03266d7c736b3
Andrei Miculischii <magtux73@hotmail.com>
2022-07-14 14:20:01 +0300

    yes

--- WebBooks/catalog/views.py
+++ WebBooks/catalog/views.py
@@ -26,7 +26,7 @@ def index(request):
 
 class BookListView(generic.ListView):
     model = Book
-    paginate_by = 3
+    paginate_by = 4
 
 class BookDetailView(generic.DetailView):
     model = Book
